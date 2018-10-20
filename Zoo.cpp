31972b86d84b84f15c1d59dcc6d37a6c1218952b
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-10-20 13:14:26 -0600

    Extend play functionality

--- Zoo.cpp
+++ Zoo.cpp
@@ -9,8 +9,18 @@ using std::endl;
 
 Zoo::Zoo() {
   penguins = new Penguin[10];
+  setPenguinArraySize(10);
+  setPenguins(0);
+
   tigers = new Tiger[10];
+  setTigerArraySize(10);
+  setTigers(0);
+
   turtles = new Turtle[10];
+  setTurtleArraySize(10);
+  setTurtles(0);
+
+  setMoney(100000);
   initMenus();
 }
 
@@ -27,13 +37,16 @@ void Zoo::play() {
   unsigned seed = time(0);
   srand(seed);
 
-  cout << "Welcome to your zoo!" endl;
+  cout << "Welcome to your zoo!" << endl;
+  cout << "You have $" << getMoney() << " to populate your crappy little zoo." << endl;
 
   setPenguins(buyAnimalMenu.getIntChoiceFromPrompt("How many penguins would you like to buy?", 1, 2, false));
   setTigers(buyAnimalMenu.getIntChoiceFromPrompt("How many tigers would you like to buy?", 1, 2, false));
   setTurtles(buyAnimalMenu.getIntChoiceFromPrompt("How many turtles would you like to buy?", 1, 2, false));
-  
-  std::cout << "Number of penguins, tigers and turtles: " << getPenguins() << ", " << getTigers() << ", " << getTurtles();
+
+  buyAnimalMenu.getIntChoiceFromPrompt("Select an option below to purchase a new animal today:", 1, buyAnimalMenu.getMenuChoices(), true);
+  //std::cout << "Number of penguins, tigers and turtles: "
+  //          << getPenguins() << ", " << getTigers() << ", " << getTurtles() << endl;
 
 }
 
@@ -45,6 +58,136 @@ void Zoo::play() {
 ***********************************************************************************************/
 void Zoo::initMenus() {
 
+  buyAnimalMenu.addMenuItem("Penguin ($1,000)");
+  buyAnimalMenu.addMenuItem("Tiger   ($10,000)");
+  buyAnimalMenu.addMenuItem("Turtle  ($100)");
+  buyAnimalMenu.addMenuItem("Skip this turn");
+
+}
+
+Animal* Zoo::doubleArray(Animal *array, int numOfAnimals, int arraySize) {
+  if (array) {
+
+    if (numOfAnimals >= arraySize) {
+
+      Animal *newArray = new Animal[arraySize * 2];
+
+      for (int i = 0; i < numOfAnimals; i++) {
+        newArray[i] = array[i];
+      }
+
+      delete [] array;
+      return newArray;
+
+    } else {
+      return array;
+    }
+
+  }
+
+}
+
+
+void Zoo::bonusDay() {
+  cout << "It's a holiday!" << endl;
+  int bonus = rand() % 250 + 250;
+
+}
+
+
+void Zoo::buyAnimal(Animal *animalToBuy, int qty) {
+
+  if (animalToBuy) {
+
+    if (getMoney() > animalToBuy->getCost()) {
+      setMoney(getMoney() - animalToBuy->getCost());
+      //Update number of correct type of animal, add to array
+
+    }
+
+  }
+
+}
+
+
+void Zoo::feedAnimals() {
+
+  for (int i = 0; i < numPenguins; i ++) {
+    setMoney(getMoney() - penguins[i].getBaseFoodCost());
+  }
+
+  for (int i = 0; i < numTigers; i ++) {
+    setMoney(getMoney() - tigers[i].getBaseFoodCost());
+  }
+  
+  for (int i = 0; i < numTurtles; i ++) {
+    setMoney(getMoney() - turtles[i].getBaseFoodCost());
+  }
+
+}
+
+
+void Zoo::increaseAge() {
+
+  for (int i = 0; i < numPenguins; i ++) {
+    penguins[i].increaseAge();
+  }
+
+  for (int i = 0; i < numTigers; i ++) {
+    tigers[i].increaseAge();
+  }
+
+  for (int i = 0; i < numTurtles; i ++) {
+    turtles[i].increaseAge();
+  }
+
+  cout << "They grow up so fast!" << endl;
+}
+
+
+void Zoo::nextTurn() {
+  //A day will follow this flow: Age -> Feed -> Random Event -> Payoff -> Purchase Adult
+  increaseAge();
+  feedAnimals();
+  randomEvent();
+  receivePayoff();
+}
+
+
+void Zoo::randomEvent() {
+
+  int event = rand() % 3;
+  cout << "Random event: " << event << endl;
+  switch (event) {
+
+    case Sickness   :
+                      break;
+    case Attendance : bonusDay();
+                      break;
+    case Birth      :
+                      break;
+    case Nothing    : cout << "Nothing to report today." << endl;
+
+  }
+
+
+}
+
+
+void Zoo::receivePayoff() {
+
+  for (int i = 0; i < numPenguins; i ++) {
+    setMoney(getMoney() + penguins[i].getPayoff());
+  }
+
+  for (int i = 0; i < numTigers; i ++) {
+    setMoney(getMoney() + tigers[i].getPayoff());
+  }
+
+  for (int i = 0; i < numTurtles; i ++) {
+    setMoney(getMoney() + turtles[i].getPayoff());
+  }
+
 }
 
 
@@ -52,8 +195,7 @@ bool Zoo::isAdult(Animal *zooAnimal) {
 
   if (zooAnimal) {
 
-    if (zooAnimal->getAge() > 2) {
-
+    if (zooAnimal->getAge() >= 3) {
       return true;
     } else {
       return false;
@@ -102,4 +244,34 @@ int Zoo::getTurtles() {
 
 void Zoo::setTurtles(int newTurtles) {
   numTurtles = newTurtles;
+}
+
+
+int Zoo::getPenguinArraySize() {
+  return penguinArraySize;
+}
+
+
+void Zoo::setPenguinArraySize(int newArraySize) {
+  penguinArraySize = newArraySize;
+}
+
+
+int Zoo::getTigerArraySize() {
+  return tigerArraySize;
+}
+
+
+void Zoo::setTigerArraySize(int newArraySize) {
+  tigerArraySize = newArraySize;
+}
+
+
+int Zoo::getTurtleArraySize() {
+  return turtleArraySize;
+}
+
+
+void Zoo::setTurtleArraySize(int newArraySize) {
+  turtleArraySize = newArraySize;
 }
--- Zoo.hpp
+++ Zoo.hpp
@@ -11,17 +11,25 @@
 #include "Tiger.hpp"
 #include "Turtle.hpp"
 
+enum Event { Sickness, Attendance, Birth, Nothing };
+
 class Zoo {
 
 private:
   Menu buyAnimalMenu;
-  int money, numPenguins, numTigers, numTurtles;
+  int money, numPenguins, numTigers, numTurtles,
+      penguinArraySize, tigerArraySize, turtleArraySize;
   Animal *penguins, *tigers, *turtles;
   void initMenus();
+  Animal* doubleArray(Animal *array, int numOfAnimals, int arraySize);
 
-  void buyAnimal();
+  void bonusDay();
+  void buyAnimal(Animal* animalToBuy, int qty);
+  void feedAnimals();
+  void increaseAge();
   void nextTurn();
   void randomEvent();
+  void receivePayoff();
 
   bool isAdult(Animal *zooAnimal);
   int getMoney();
@@ -32,6 +40,12 @@ private:
   void setTigers(int newTigers);
   int getTurtles();
   void setTurtles(int newTurtles);
+  int getPenguinArraySize();
+  void setPenguinArraySize(int newArraySize);
+  int getTigerArraySize();
+  void setTigerArraySize(int newArraySize);
+  int getTurtleArraySize();
+  void setTurtleArraySize(int newArraySize);
 
 public:
   Zoo();
