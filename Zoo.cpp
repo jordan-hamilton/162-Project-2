ced3d0e6386249d05b81494bb47db7e428c447a6
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-10-21 12:51:18 -0600

    Remove debug lines, decrease starting money

--- Zoo.cpp
+++ Zoo.cpp
@@ -20,7 +20,7 @@ Zoo::Zoo() {
   setTurtleArraySize(10);
   setTurtles(0);
 
-  setMoney(100000);
+  setMoney(40000);
   initMenus();
 }
 
@@ -71,11 +71,9 @@ int qtyAdded = 0;
                       penguins = validateArraySize(penguins, penguin, getPenguins(), getPenguinArraySize());
 
                       for (int i = 0; (i < getPenguinArraySize() && qtyAdded < qty); i++) {
-                        cout << "Checking the age of penguin " << i << endl;
+
                         if (penguins[i].getAge() < 0) {
-                          cout << "This penguin's age is " << penguins[i].getAge() << endl;
                           penguins[i].setAge(age);
-                          cout << "We've updated the age to " << penguins[i].getAge() << endl;
 
                           if (wasPurchased) {
                             setMoney(getMoney() - penguins[i].getCost());
@@ -85,18 +83,16 @@ int qtyAdded = 0;
                         }
 
                       }
-                      cout << "We added " << qtyAdded << " penguins to the zoo." << endl;
+
                       break;
 
     case tiger      : setTigers(getTigers() + qty);
                       tigers = validateArraySize(tigers, tiger, getTigers(), getTigerArraySize());
 
                       for (int i = 0; (i < getTigerArraySize() && qtyAdded < qty); i++) {
-                        cout << "Checking the age of tiger " << i << endl;
+
                         if (tigers[i].getAge() < 0) {
-                          cout << "This tiger's age is " << tigers[i].getAge() << endl;
                           tigers[i].setAge(age);
-                          cout << "We've updated the age to " << tigers[i].getAge() << endl;
 
                           if (wasPurchased) {
                             setMoney(getMoney() - tigers[i].getCost());
@@ -106,18 +102,16 @@ int qtyAdded = 0;
                         }
 
                       }
-                      cout << "We added " << qtyAdded << " tigers to the zoo." << endl;
+
                       break;
 
     case turtle     : setTurtles(getTurtles() + qty);
                       turtles = validateArraySize(turtles, turtle, getTurtles(), getTurtleArraySize());
 
                       for (int i = 0; (i < getTurtleArraySize() && qtyAdded < qty); i++) {
-                        cout << "Checking the age of turtle " << i << endl;
+
                         if (turtles[i].getAge() < 0) {
-                          cout << "This turtle's age is " << turtles[i].getAge() << endl;
                           turtles[i].setAge(age);
-                          cout << "We've updated the age to " << turtles[i].getAge() << endl;
 
                           if (wasPurchased) {
                             setMoney(getMoney() - turtles[i].getCost());
@@ -127,7 +121,7 @@ int qtyAdded = 0;
                         }
 
                       }
-                      cout << "We added " << qtyAdded << " turtles to the zoo." << endl;
+
                       break;
 
     default         : {}
@@ -138,24 +132,34 @@ int qtyAdded = 0;
 
 
 /***********************************************************************************************
-** Description: Loops through each array and subtracts the player's current money by the base
-** food cost for each of the owned animals owned. We're assuming all animals of the same derived
-** class have the same base food cost, so we're not conditionally subtracting the cost by only
-** searching for valid animals in the arrays with ages of 0 or greater. Instead, we subtract
-** according to the number of animals we've tracked with numPenguins, numTigers, and numTurtles.
+** Description: Loops through each animal array and subtracts the base food cost for each animal
+** owned from the player's current money. We search for valid animals in each array by first
+** confirming that the age of the animal at an index is at least 0.
 ***********************************************************************************************/
 void Zoo::feedAnimals() {
 
-  for (int i = 0; i < numPenguins; i++) {
-    setMoney(getMoney() - penguins[i].getBaseFoodCost());
+  for (int i = 0; i < getPenguinArraySize(); i++) {
+
+    if (penguins[i].getAge() >= 0) {
+      setMoney(getMoney() - penguins[i].getBaseFoodCost());
+    }
+
   }
 
-  for (int i = 0; i < numTigers; i++) {
-    setMoney(getMoney() - tigers[i].getBaseFoodCost());
+  for (int i = 0; i < getTigerArraySize(); i++) {
+
+    if (tigers[i].getAge() >= 0) {
+      setMoney(getMoney() - tigers[i].getBaseFoodCost());
+    }
+
   }
 
-  for (int i = 0; i < numTurtles; i++) {
-    setMoney(getMoney() - turtles[i].getBaseFoodCost());
+  for (int i = 0; i < getTurtleArraySize(); i++) {
+
+    if (turtles[i].getAge() >= 0) {
+      setMoney(getMoney() - turtles[i].getBaseFoodCost());
+    }
+
   }
 
 }
@@ -172,7 +176,6 @@ void Zoo::increaseAge() {
 
     if (penguins[i].getAge() >= 0) {
       penguins[i].increaseAge();
-      cout << "Age of penguin " << i << ": " << penguins[i].getAge() << endl;
     }
 
   }
@@ -181,7 +184,6 @@ void Zoo::increaseAge() {
 
     if (tigers[i].getAge() >= 0) {
       tigers[i].increaseAge();
-      cout << "Age of tiger " << i << ": " << tigers[i].getAge() << endl;
     }
 
   }
@@ -190,7 +192,6 @@ void Zoo::increaseAge() {
 
     if (turtles[i].getAge() >= 0) {
       turtles[i].increaseAge();
-      cout << "Age of turtle " << i << ": " << turtles[i].getAge() << endl;
     }
 
   }
@@ -217,7 +218,7 @@ void Zoo::initMenus() {
 
 
 /***********************************************************************************************
-** Description: Takes a reference to an animal and calls its getAge function to determine
+** Description: Takes a reference to an animal and calls the getAge function to determine
 ** whether or not this animal is an adult and can have babies. Returns a boolean, with true
 ** indicating that the animal is an adult.
 ***********************************************************************************************/
@@ -250,9 +251,14 @@ bool Zoo::nextTurn() {
 }
 
 
+/***********************************************************************************************
+** Description: Prompts the user to purchase an animal or skip their turn and not purchase an
+** adult animal. If the user chooses to buy an animal, the addAnimal function is called in the
+** switch statement, specifying a purchase of one of the selected animal that is 3 days old.
+***********************************************************************************************/
 void Zoo::purchaseAdultPrompt() {
 
-  int animalToAdd = buyAnimalMenu.getIntChoiceFromPrompt("Select an option below to purchase an adult animal today:", 1, buyAnimalMenu.getMenuChoices(), true);
+  int animalToAdd = buyAnimalMenu.getIntChoiceFromPrompt("\nSelect an option below to purchase an adult animal today:", 1, buyAnimalMenu.getMenuChoices(), true);
 
   switch (animalToAdd) {
 
@@ -268,16 +274,95 @@ void Zoo::purchaseAdultPrompt() {
 
 }
 
-
+/***********************************************************************************************
+** Description: Assigns a random number between 1 and 3 to pregnantAnimal to determine which
+** type of animal will have babies during this random event. Depending on the value in the
+** pregnantAnimal variable, the proper array is looped through, calling the isAdult function
+** until the entire array is looped through or an adult animal is found. If one is found,
+** the number of babies associated with that derived class are added to the array using the
+** addAnimal function. If no adults are found, no babies are born during this turn.
+***********************************************************************************************/
 void Zoo::randomBirth() {
-  cout << "Someone's having babies!" << endl;
+
+  int numOfAdults = 0;
+
+  int pregnantAnimal = rand() % 3 + 1;
+
+  switch (pregnantAnimal) {
+
+    case penguin    : for (int i = 0; (i < getPenguinArraySize() && numOfAdults < 1); i++) {
+
+                        if (isAdult(penguins[i])) {
+
+                          addAnimal(penguin, penguins[i].getNumberOfBabies(), 0, false);
+                          numOfAdults++;
+                          cout << penguins[i].getNumberOfBabies() << " penguins were born!" << endl;
+
+                        }
+
+                      }
+
+                      break;
+
+    case tiger      : for (int i = 0; (i < getTigerArraySize() && numOfAdults < 1); i++) {
+
+                        if (isAdult(tigers[i])) {
+
+                          addAnimal(tiger, tigers[i].getNumberOfBabies(), 0, false);
+                          numOfAdults++;
+                          cout << tigers[i].getNumberOfBabies() << " tiger was born!" << endl;
+
+                        }
+
+                      }
+
+                      break;
+
+    case turtle     : for (int i = 0; (i < getTurtleArraySize() && numOfAdults < 1); i++) {
+
+                        if (isAdult(turtles[i])) {
+
+                          addAnimal(turtle, turtles[i].getNumberOfBabies(), 0, false);
+                          numOfAdults++;
+                          cout << turtles[i].getNumberOfBabies() << " turtles were born!" << endl;
+
+                        }
+
+                      }
+
+                      break;
+
+    default         : {}
+
+  }
+
+
 }
 
 
+/***********************************************************************************************
+** Description: Assigns a random number between 250 and 500 to bonus to determine how much to
+** of a bonus per tiger to add to the player's money during this random event. If the numTigers
+** data member indicated there is at least one tiger, the result of the bonus is output to the
+** screen. Then, the tigers array is looped through to find valid tigers (those with an age of
+** 0 or greater) to add the bonus to the player's money for each valid tiger object.
+***********************************************************************************************/
 void Zoo::randomBonus() {
 
   int bonus = rand() % 251 + 250;
-  setMoney(getMoney() + numTigers * bonus);
+
+  if (getTigers() > 0) {
+    cout << "Attendance increase! You collected an additional $"
+         << bonus * getTigers() << " in ticket sales today." << endl;
+  }
+
+  for (int i = 0; i < getTigerArraySize(); i++) {
+
+    if (tigers[i].getAge() >= 0) {
+      setMoney(getMoney() + bonus);
+    }
+
+  }
 
 }
 
@@ -285,7 +370,7 @@ void Zoo::randomBonus() {
 void Zoo::randomEvent() {
 
   int event = rand() % 4;
-  cout << "Random event: " << event << endl;
+
   switch (event) {
 
     case sickness   : randomSickness();
@@ -302,25 +387,48 @@ void Zoo::randomEvent() {
 }
 
 
+/***********************************************************************************************
+** Description: Assigns a random number between 1 and 3 to sickAnimal to determine which
+** type of animal will become sick during this random event. Depending on the value in the
+** sickAnimal variable, the switch statement checks that there is at least one of this type of
+** animal, then sets the age of the last valid animal in the array to an invalid value (-1).
+** The numPenguins, numTigers or numTurtles data member is decremented by one with a setter
+** function and the result is output to the screen.
+***********************************************************************************************/
 void Zoo::randomSickness() {
 
   int sickAnimal = rand() % 3 + 1;
 
   switch (sickAnimal) {
 
-    case penguin    : penguins[getPenguins() - 1].setAge(-1);
-                      setPenguins(getPenguins() - 1);
-                      cout << "A penguin just died!" << endl;
+    case penguin    : if (getPenguins() > 0) {
+
+                        penguins[getPenguins() - 1].setAge(-1);
+                        setPenguins(getPenguins() - 1);
+                        cout << "A penguin has died!" << endl;
+
+                      }
+
                       break;
 
-    case tiger      : tigers[getTigers() - 1].setAge(-1);
-                      setTigers(getTigers() - 1);
-                      cout << "A tiger just died!" << endl;
+    case tiger      : if (getTigers() > 0) {
+
+                        tigers[getTigers() - 1].setAge(-1);
+                        setTigers(getTigers() - 1);
+                        cout << "A tiger has died!" << endl;
+
+                      }
+
                       break;
 
-    case turtle     : turtles[getTurtles() - 1].setAge(-1);
-                      setTurtles(getTurtles() - 1);
-                      cout << "A turtle just died!" << endl;
+    case turtle     : if (getTurtles() > 0) {
+
+                        turtles[getTurtles() - 1].setAge(-1);
+                        setTurtles(getTurtles() - 1);
+                        cout << "A turtle has died!" << endl;
+
+                      }
+
                       break;
 
     default         : {}
@@ -333,16 +441,28 @@ void Zoo::randomSickness() {
 
 void Zoo::receivePayoff() {
 
-  for (int i = 0; i < numPenguins; i++) {
-    setMoney(getMoney() + penguins[i].getPayoff());
+  for (int i = 0; i < getPenguinArraySize(); i++) {
+
+    if (penguins[i].getAge() >= 0) {
+      setMoney(getMoney() + penguins[i].getPayoff());
+    }
+
   }
 
-  for (int i = 0; i < numTigers; i++) {
-    setMoney(getMoney() + tigers[i].getPayoff());
+  for (int i = 0; i < getTigerArraySize(); i++) {
+
+    if (tigers[i].getAge() >= 0) {
+      setMoney(getMoney() + tigers[i].getPayoff());
+    }
+
   }
 
-  for (int i = 0; i < numTurtles; i++) {
-    setMoney(getMoney() + turtles[i].getPayoff());
+  for (int i = 0; i < getTurtleArraySize(); i++) {
+
+    if (turtles[i].getAge() >= 0) {
+      setMoney(getMoney() + turtles[i].getPayoff());
+    }
+
   }
 
 }
